d42e7b9f5a8d604c04aece7a5615427912fb24b6
Dave Keen <dev@ruffness.com>
2018-12-05 09:14:43 +0100

    Updated to 0.19

--- src/Focus.elm
+++ src/Focus.elm
@@ -1,4 +1,10 @@
-module Focus exposing (Focus, get, set, update, (=>), create)
+module Focus exposing
+    ( Focus
+    , get, set, update
+    , compose
+    , create
+    )
+
 {-| Our goal is to update a field deep inside some nested records. For example,
 if we want to add one to `object.physics.velocity.x` or set it to zero, we would
 be writing code like this:
@@ -14,22 +20,31 @@ quality in that it can help you to be lax with abstraction boundaries.
 
 By making it easy to look deep inside of data structures, it encourages you to
 stop thinking about how to make these substructures modular, perhaps leading
-to messier architecture *and* some extra conceptual complexity. It may also
+to messier architecture _and_ some extra conceptual complexity. It may also
 make your code slower by encouraging you to take many passes over data,
 creating lots of intermediate data structures for no particular reason.
-*Use with these risk in mind!*
+_Use with these risk in mind!_
+
 
 # Focus
+
 @docs Focus
 
+
 # Get, Set, Update
+
 @docs get, set, update
 
+
 # Compose Foci
-@docs (=>)
+
+@docs compose
+
 
 # Create your own Focus
+
 @docs create
+
 -}
 
 
@@ -38,11 +53,11 @@ Maybe this means a certain field in a record or a certain element in an array.
 The focus then lets you `get`, `set`, and `update` this small part of a big
 value.
 -}
-type Focus big small =
-  Focus
-    { get : big -> small
-    , update : (small -> small) -> big -> big
-    }
+type Focus big small
+    = Focus
+        { get : big -> small
+        , update : (small -> small) -> big -> big
+        }
 
 
 {-| A `Focus` is a value. It describes a strategy for getting and updating
@@ -50,8 +65,8 @@ things. This function lets you define a `Focus` yourself by providing a `get`
 function and an `update` function.
 -}
 create : (big -> small) -> ((small -> small) -> big -> big) -> Focus big small
-create get update =
-  Focus { get = get, update = update }
+create newGet newUpdate =
+    Focus { get = newGet, update = newUpdate }
 
 
 {-| Get a small part of a big thing.
@@ -62,10 +77,11 @@ create get update =
 
 Seems sort of silly given that you can just say `.x` to do the same thing. It
 will become much more useful when we can begin to compose foci, so keep reading!
+
 -}
 get : Focus big small -> big -> small
 get (Focus focus) big =
-  focus.get big
+    focus.get big
 
 
 {-| Set a small part of a big thing.
@@ -73,10 +89,11 @@ get (Focus focus) big =
     x : Focus { record | x:a } a
 
     set x 42 { x=3, y=4 } == { x=42, y=4 }
+
 -}
 set : Focus big small -> small -> big -> big
 set (Focus focus) small big =
-  focus.update (always small) big
+    focus.update (always small) big
 
 
 {-| Update a small part of a big thing.
@@ -97,10 +114,11 @@ This lets us chain updates without any special record syntax:
 The downside of this approach is that this means we take two passes over the
 record, whereas normal record syntax would only have required one. It may be
 best to use a mix `Focus` and typical record updates to minimize traversals.
+
 -}
 update : Focus big small -> (small -> small) -> big -> big
 update (Focus focus) f big =
-  focus.update f big
+    focus.update f big
 
 
 
@@ -126,21 +144,21 @@ makes this library worthwhile, but also what makes it dangerous. You will be
 doing a lot of silly work if you start writing code like this:
 
     object
-      |> set (physics => velocity => x) 0
-      |> set (physics => velocity => y) 0
+        |> set (physics => velocity => x) 0
+        |> set (physics => velocity => y) 0
 
 It is pretty, but you pay for it in performance because you take two passes
 over `object` instead of one. It may be best to do the last step with typical
 record updates so that this can be done in one pass.
+
 -}
-(=>) : Focus big medium -> Focus medium small -> Focus big small
-(=>) (Focus largerFocus) (Focus smallerFocus) =
-  let
-    get big =
-      smallerFocus.get (largerFocus.get big)
+compose : Focus big medium -> Focus medium small -> Focus big small
+compose (Focus largerFocus) (Focus smallerFocus) =
+    let
+        newGet big =
+            smallerFocus.get (largerFocus.get big)
 
-    update f big =
-      largerFocus.update (smallerFocus.update f) big
-  in
-    Focus { get = get, update = update }
-
+        newUpdate f big =
+            largerFocus.update (smallerFocus.update f) big
+    in
+    Focus { get = newGet, update = newUpdate }
